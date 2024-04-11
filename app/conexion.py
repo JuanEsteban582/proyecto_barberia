49aa6e7364955fd4c51673c3301fd2055c217a6d
JuanEsteban582 <juanycamila582@gmail.com>
2024-04-11 04:51:14 -0500

    comiit_ultimo

--- app/conexion.py
+++ app/conexion.py
@@ -15,8 +15,8 @@ app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes = 30)
 
 app.config['MYSQL_DATABASE_HOST'] ='localhost'
 app.config['MYSQL_DATABASE_PORT'] = 3306 #servidor
-app.config['MYSQL_DATABASE_USER'] ='id22030536_barberlocate' #el usuario en xampp de administrador
-app.config ['MYSQL_DATABASE_PASSWORD'] ='Barber_locate1234' #contrasaña de la base de datos si le tiene
-app.config ['MYSQL_DATABASE_DB'] ='id22030536_barberia1' 
+app.config['MYSQL_DATABASE_USER'] ='root' #el usuario en xampp de administrador
+app.config ['MYSQL_DATABASE_PASSWORD'] ='' #contrasaña de la base de datos si le tiene
+app.config ['MYSQL_DATABASE_DB'] ='barberia1' 
 app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'uploads')
 mysql.init_app(app)
